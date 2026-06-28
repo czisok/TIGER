6de2f40853f526d582d846cdf8e98511ac6816ef
zhangbo.999 <zhangbo.999@bytedance.com>
2026-06-28 11:25:26 +0800

    up
    Change-Id: Ib0806686af17b5712d417293de84aad62021fdf6

--- rqvae/main.py
+++ rqvae/main.py
@@ -9,7 +9,8 @@ from torch.utils.data import DataLoader
 
 from datasets import EmbDataset
 from models.rqvae import RQVAE
-from trainer import  Trainer
+from trainer import Trainer
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Index")
@@ -34,13 +35,13 @@ def parse_args():
 
     parser.add_argument("--device", type=str, default="cuda:0", help="gpu or cpu")
 
-    parser.add_argument('--num_emb_list', type=int, nargs='+', default=[256,256,256], help='emb num of every vq')
+    parser.add_argument('--num_emb_list', type=int, nargs='+', default=[256, 256, 256], help='emb num of every vq')
     parser.add_argument('--e_dim', type=int, default=32, help='vq codebook embedding size')
     parser.add_argument('--quant_loss_weight', type=float, default=1.0, help='vq quantion loss weight')
     parser.add_argument("--beta", type=float, default=0.25, help="Beta for commitment loss")
-    parser.add_argument('--layers', type=int, nargs='+', default=[512,256,128,64], help='hidden sizes of every layer')
+    parser.add_argument('--layers', type=int, nargs='+', default=[512, 256, 128, 64], help='hidden sizes of every layer')
     parser.add_argument('--save_limit', type=int, default=5, help='save limit for ckpt')
-    
+
     parser.add_argument("--ckpt_dir", type=str, default="./ckpt/Beauty", help="please specify output directory for model")
 
     return parser.parse_args()
@@ -80,12 +81,11 @@ if __name__ == '__main__':
                   sk_iters=args.sk_iters,
                   )
     print(model)
-    data_loader = DataLoader(data,num_workers=args.num_workers,
+    data_loader = DataLoader(data, num_workers=args.num_workers,
                              batch_size=args.batch_size, shuffle=True,
                              pin_memory=True)
-    trainer = Trainer(args,model, len(data_loader))
+    trainer = Trainer(args, model, len(data_loader))
     best_loss, best_collision_rate = trainer.fit(data_loader)
 
-    print("Best Loss",best_loss)
+    print("Best Loss", best_loss)
     print("Best Collision Rate", best_collision_rate)
-
